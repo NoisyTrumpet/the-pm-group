1c377b4dac67b336d0b5b3f995d54e251b5e6af9
Cesar Benavides <cesarbenavides@icloud.com>
2022-06-14 14:53:43 -0500

    Adding correct URL for grav forms client

--- src/hooks/apolloClient.tsx
+++ src/hooks/apolloClient.tsx
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client"
 import { createUploadLink } from "apollo-upload-client"
 
-const myEndpoint = process.env.GATSBY_WPGRAPHQL_URL
+const myEndpoint = process.env.GATSBY_WORDPRESS_API_URL
 
 export const client = new ApolloClient({
   uri: myEndpoint,
