febaca807c47b68d50497c51bd5ab4993e38a258
Cesar Benavides <cesarbenavides@icloud.com>
2022-09-16 21:01:34 -0500

    font preload

--- gatsby-shared.tsx
+++ gatsby-shared.tsx
@@ -17,7 +17,7 @@ export const wrapPageElement:
     <link
       rel="preload"
       key="preload-montserrat-latin-500"
-      href="/static/montserrat-latin-500-normal-f41a91ec0ef285c804abfbf08972d7de.woff2"
+      href="/static/montserrat-latin-500-normal-38a674fae449557fef97f120c301ff8d.woff2"
       as="font"
       type="font/woff2"
       crossOrigin="anonymous"
@@ -25,7 +25,15 @@ export const wrapPageElement:
     <link
       rel="preload"
       key="preload-montserrat-latin-700"
-      href="/static/montserrat-latin-700-normal-145c46aabb2eccdd1f7bfb2983b6d5e4.woff2"
+      href="/static/montserrat-latin-700-normal-56c09b9a46af6a54eeb30e554f833d13.woff2"
+      as="font"
+      type="font/woff2"
+      crossOrigin="anonymous"
+    />
+    <link
+      rel="preload"
+      key="preload-montserrat-latin-600"
+      href="/static/montserrat-latin-600-normal-5d6df38dd70f631c488bdec905f68464.woff2"
       as="font"
       type="font/woff2"
       crossOrigin="anonymous"
