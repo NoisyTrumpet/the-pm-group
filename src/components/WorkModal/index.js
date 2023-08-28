95ba60d7a328e0ac4bb8d6fd07767628f9a2d400
Ray Anthony <16364682+Ray-Anthony@users.noreply.github.com>
2023-08-28 15:41:29 -0500

    Update videoId variable in iframe source

--- src/components/WorkModal/index.js
+++ src/components/WorkModal/index.js
@@ -68,7 +68,7 @@ const WorkModal = ({ title, id, videoLink, isVideo, image, children }) => {
               <iframe
                 width="100%"
                 height="400px"
-                src={`https://www.youtube.com/embed/${videoId[1]}?&autoplay=1`}
+                src={`https://www.youtube.com/embed/${videoId}?&autoplay=1`}
                 title={title}
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
